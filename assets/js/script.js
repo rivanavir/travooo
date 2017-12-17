e3d1215a772650228751fade809503417dd6b959
Ivan <rivanyshka@gmail.com>
2017-12-17 13:53:53 +0200

    posts block commit

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,4 +4,20 @@ $(document).ready(function(){
     $(this).toggleClass('checked-block');
   });
 
+  let slider = $("#lightSlider").lightSlider({
+    item:1,
+    pager: false,
+    controls: false,
+    loop: true
+  });
+
+  $('.side-right-control .slide-link').on('click', function(e){
+    e.preventDefault();
+    if($(this).hasClass('slide-prev')){
+      slider.goToPrevSlide();
+    } else if($(this).hasClass('slide-next')){
+      slider.goToNextSlide();
+    }
+  })
+
 });
